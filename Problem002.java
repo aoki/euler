1fa108825365ee1d0bccb47d9383b9b45fa425e8
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-15 11:18:59 +0900

    Problem002

---
+++ Problem002.java
@@ -0,0 +1,15 @@
+public class Problem002 {
+
+	public int compute(int max){
+		int ans = 0;
+		for(int prev=1, actual=2; actual<max; actual+=prev, prev=actual-prev){
+			if(actual % 2 == 0) ans += actual;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Answer is " + new Problem002().compute(4000000));
+	}
+
+}
